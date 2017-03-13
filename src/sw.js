8ca58439e412e48de7c2485a47458027736917d7
Alejandro Antivero <alejandro.antivero@gmail.com>
2017-03-12 22:40:21 -0300

    Service worker

---
+++ src/sw.js
@@ -0,0 +1,61 @@
+/**
+ * Created by alejandro on 12/3/2017.
+ */
+
+let log = console.log.bind(console);
+let err = console.error.bind(console);
+
+let version = 1;
+let cacheName = 'debin1-pwa-client-v' + version;
+let dataCacheName = 'debin1-pwa-client-data-v' + version;
+let appShellFilesToCache = [
+  './',
+  './index.html',
+  './inline.bundle.js',
+  './styles.bundle.js',
+  './vendor.bundle.js',
+  './main.bundle.js'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(self.skipWaiting());
+  log('Service Worker: Installed');
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      log('Service Worker: Caching App Shell');
+      return cache.addAll(appShellFilesToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(self.clients.claim());
+  log('Service Worker: Activate');
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+
+        if (key !== cacheName) {
+          log('Service Worker: Remove old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  log('Service Worker: Fetch URL', e.request.url);
+
+  e.respondWith(
+    caches.match(e.request.clone()).then((response) => {
+      return response || fetch(e.request.clone()).then((r2) => {
+          return caches.open(dataCacheName).then((cache) => {
+            console.log('Service Worker: Fetched & Cached URL ', e.request.url);
+            cache.put(e.request.url, r2.clone());
+            return r2.clone();
+          });
+        });
+    })
+  );
+});
